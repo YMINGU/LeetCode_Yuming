6a568f588bc0a4c2025ade215fcadfc6e8c724c0
Yuming Gu <yug52@pitt.edu>
2022-07-23 14:07:14 -0400

    Create 383. Ransom Note.java

---
+++ 383. Ransom Note.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int arr[]=new int [26];
+        for (int i=0;i<magazine.length();i++){
+            arr[magazine.charAt(i)-'a']++;
+        }
+        for (int j=0;j<ransomNote.length();j++){
+            if(arr[ransomNote.charAt(j)-'a']-1<0){
+                return false;
+            }
+            arr[ransomNote.charAt(j)-'a']--;
+        }
+        return true;
+        
+    }
+}
