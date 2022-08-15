883f24f4b1d699f2e11324377972ee4b800b0d77
Yuming Gu <yug52@pitt.edu>
2022-08-15 12:59:29 -0400

    Update 941. Valid Mountain Array.java

--- 941. Valid Mountain Array.java
+++ 941. Valid Mountain Array.java
@@ -30,3 +30,22 @@ class Solution {
         
     }
 }
+
+-------------------------------------------------------
+
+class Solution {
+    public boolean validMountainArray(int[] arr) {
+        int i=0;
+        int N=arr.length;
+        while(i+1<N && arr[i]<arr[i+1]){
+            i++;
+        }
+        if(i==0 || i==N-1){
+            return false;
+        }
+        while (i+1<N && arr[i]>arr[i+1]){
+            i++;
+        }
+        return i==N-1;        
+    }
+}
