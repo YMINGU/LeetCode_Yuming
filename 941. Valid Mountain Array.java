407fd65255dc7ef72350c14e9c68de826b607cb6
Yuming Gu <yug52@pitt.edu>
2022-08-15 12:47:54 -0400

    Create 941. Valid Mountain Array.java

---
+++ 941. Valid Mountain Array.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean validMountainArray(int[] arr) {
+        boolean result=false;
+        boolean p1=true;
+        boolean p2=true;
+        if(arr.length>=3){
+            for(int i=1;i<arr.length-1;i++){
+                for(int j=i;j>0;j--){
+                    if(arr[j]-arr[j-1]<=0){
+                        p1=false;
+                        break;
+                    }
+                }
+                for(int k=i;k<arr.length-1;k++){
+                    if(arr[k]-arr[k+1]<=0){
+                        p2=false;
+                        break;
+                    }
+                }
+                if(p1 && p2){
+                    result=true;
+                }
+                else{
+                    p1=true;
+                    p2=true;
+                }
+            }
+        }
+        return result;
+        
+    }
+}
