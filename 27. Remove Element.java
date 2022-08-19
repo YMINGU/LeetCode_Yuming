5d9205f17c9e37b8eaf325aa3be9c638f3034566
Yuming Gu <yug52@pitt.edu>
2022-08-19 15:59:52 -0400

    Update 27. Remove Element.java

--- 27. Remove Element.java
+++ 27. Remove Element.java
@@ -25,3 +25,21 @@ class Solution {
     }
     return i;
 }
+
+----------------------------------------------------------
+    class Solution {
+    public int removeElement(int[] nums, int val) {
+        int temp=0;
+        int j=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=val){
+                temp=nums[j];
+                nums[j]=nums[i];
+                nums[i]=temp;
+                j++;
+            }
+        }
+        return j;
+        
+    }
+}
