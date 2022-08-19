3c67fa17a340b0437e9567c4f0b5ee130339b320
Yuming Gu <yug52@pitt.edu>
2022-08-19 17:02:00 -0400

    Update 1051. Height Checker.java

--- 1051. Height Checker.java
+++ 1051. Height Checker.java
@@ -15,3 +15,31 @@ class Solution {
         
     }
 }
+
+
+-----------------------------------------------------
+class Solution {
+    public int heightChecker(int[] heights) {
+        int[] heightToFreq = new int[101];
+        
+        for (int height : heights) {
+            heightToFreq[height]++;
+        }
+        
+        int result = 0;
+        int curHeight = 0;
+        
+        for (int i = 0; i < heights.length; i++) {
+            while (heightToFreq[curHeight] == 0) {
+                curHeight++;
+            }
+            
+            if (curHeight != heights[i]) {
+                result++;
+            }
+            heightToFreq[curHeight]--;
+        }
+        
+        return result;
+    }
+}
