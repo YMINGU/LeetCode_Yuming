ca1a6b0faaa267c1e324cbcd00cb845c7eff95de
Yuming Gu <yug52@pitt.edu>
2022-09-29 00:30:05 -0400

    Update 253. Meeting Rooms II.java

--- 253. Meeting Rooms II.java
+++ 253. Meeting Rooms II.java
@@ -110,3 +110,17 @@ class Solution {
     return usedRooms;
   }
 }
+
+-----------------------------------------------------------------------------------------------
+    public int minMeetingRooms(int[][] intervals) {
+        Map<Integer, Integer> m = new TreeMap<>();
+        for (int[] t : intervals) {
+            m.put(t[0], m.getOrDefault(t[0], 0) + 1);
+            m.put(t[1], m.getOrDefault(t[1], 0) - 1);
+        }
+        int res = 0, cur = 0;
+        for (int v : m.values()) {
+            res = Math.max(res, cur += v);
+        }
+        return res;
+    }
