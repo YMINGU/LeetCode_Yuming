594255c63463a62035f9b8b21653d1078e6990eb
Yuming Gu <yug52@pitt.edu>
2023-08-23 21:09:08 -0700

    Update 1. Two Sum.java

--- 1. Two Sum.java
+++ 1. Two Sum.java
@@ -14,3 +14,47 @@ class Solution {
         return result;     
     }
 }
+
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i=0;i<nums.length;i++){
+            for (int j=i+1;j<nums.length;j++){
+                if(nums[j]==target-nums[i]){
+                    return new int[] {i,j};
+                }
+            }
+        }
+        return null;
+    }
+}
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> map=new HashMap<>();
+        for (int i=0;i<nums.length;i++){
+            map.put(nums[i],i);
+        }
+        for (int i=0;i<nums.length;i++){
+            int complement=target-nums[i];
+            if(map.containsKey(complement) && map.get(complement)!=i){
+                return new int[]{i,map.get(complement)};
+            }
+        }
+        return null;  
+    }
+}
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            int comp=target-nums[i];
+            if(map.containsKey(comp)){
+                return new int[]{map.get(comp),i};
+            }
+            map.put(nums[i],i);
+        }
+        return null;     
+    }
+}
