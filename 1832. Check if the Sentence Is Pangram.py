50cd894279f878a0988201c6f59b3c5f2e0c3d07
Yuming Gu <yug52@pitt.edu>
2024-03-03 17:15:04 -0500

    Update 1832. Check if the Sentence Is Pangram.py

--- 1832. Check if the Sentence Is Pangram.py
+++ 1832. Check if the Sentence Is Pangram.py
@@ -9,5 +9,12 @@ class Solution:
             else:
                 continue
         return True
+
+-----
+
+class Solution:
+    def checkIfPangram(self, sentence: str) -> bool:
+        seen=set(sentence)
+        return len(seen)==26
         
         
