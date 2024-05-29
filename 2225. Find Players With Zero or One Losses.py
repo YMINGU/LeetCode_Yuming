0da37b283936a5e6d716df95c41d952ba5b572aa
Yuming Gu <yug52@pitt.edu>
2024-05-29 16:40:53 -0400

    Update 2225. Find Players With Zero or One Losses.py

--- 2225. Find Players With Zero or One Losses.py
+++ 2225. Find Players With Zero or One Losses.py
@@ -15,6 +15,28 @@ class Solution:
             if key not in countslose:
                 nonlose.append(key)
         return [sorted(nonlose),sorted(onelose)]
+
+
+------------------------------------------------------------------------------
+
+from collections import defaultdict
+class Solution:
+    def findWinners(self, matches: List[List[int]]) -> List[List[int]]:
+        counts=defaultdict(int)
+        nonlose=[]
+        onelose=[]
+        for winner, loser in matches:
+            if winner not in counts:
+                counts[winner]=0
+            counts[loser]+=1
+        for key in counts:
+            if counts[key]==0:
+                nonlose.append(key)
+            if counts[key]==1:
+                onelose.append(key)
+        return [sorted(nonlose),sorted(onelose)]
+        
+        
         
         
         
