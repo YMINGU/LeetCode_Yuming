cf05dad9b50f533033eeb9a50136907c8e5d91b8
Yuming Gu <yug52@pitt.edu>
2024-05-30 16:10:25 -0400

    Update 2260. Minimum Consecutive Cards to Pick Up.py

--- 2260. Minimum Consecutive Cards to Pick Up.py
+++ 2260. Minimum Consecutive Cards to Pick Up.py
@@ -11,3 +11,15 @@ class Solution:
         return ans if ans<float("inf") else -1
 
   ----------------------------------------------------------
+
+from collections import defaultdict
+class Solution:
+    def minimumCardPickup(self, cards: List[int]) -> int:
+        dic=defaultdict(list)
+        ans=float("inf")
+        for i in range(len(cards)):
+            if cards[i] in dic:
+                ans=min(ans,i-dic[cards[i]]+1)
+            dic[cards[i]]=i
+        return ans if ans<float("inf") else -1
+        
