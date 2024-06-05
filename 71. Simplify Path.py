246d4e91b163bb39dbce7910bad15d8498aaf9c4
Yuming Gu <yug52@pitt.edu>
2024-06-04 20:17:55 -0400

    Create 71. Simplify Path.py

---
+++ 71. Simplify Path.py
@@ -0,0 +1,13 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        stack=[]
+        for c in path.split('/'):
+            if c=='..':
+                if stack:
+                    stack.pop()
+            elif c=='.' or not c:
+                continue
+            else:
+                stack.append(c)
+        return "/"+"/".join(stack)
+        
