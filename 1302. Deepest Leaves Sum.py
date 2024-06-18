4a1b551a8c06f3d35c5fcdd2c47f535a89c284e3
Yuming Gu <yug52@pitt.edu>
2024-06-18 16:35:19 -0400

    Update 1302. Deepest Leaves Sum.py

--- 1302. Deepest Leaves Sum.py
+++ 1302. Deepest Leaves Sum.py
@@ -29,6 +29,27 @@ class Solution:
                     if node.right:
                         queue.append((node.right,curr+1))
         return ans
+        --------
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from collections import deque
+class Solution:
+    def deepestLeavesSum(self, root: Optional[TreeNode]) -> int:
+        next_level=deque([root])
+        while next_level:
+            curr_level=next_level
+            next_level=deque()
+            for node in curr_level:
+                if node.left:
+                    next_level.append(node.left)
+                if node.right:
+                    next_level.append(node.right)
+        return sum([node.val for node in curr_level])
+        
                     
         
         
