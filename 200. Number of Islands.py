62aba9b6d27fb80ded586b22408531c908ad7723
Yuming Gu <yug52@pitt.edu>
2024-07-01 13:33:41 -0400

    Update 200. Number of Islands.py

--- 200. Number of Islands.py
+++ 200. Number of Islands.py
@@ -21,3 +21,29 @@ class Solution:
                     dfs(row,col)
         return ans
 -------
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        def valid(row,col):
+            return 0<=row<m and 0<=col<n and grid[row][col]=="1"
+        def dfs(start_row,start_col):
+            stack=[(start_row,start_col)]
+            while stack:
+                row,col=stack.pop()
+                for dx,dy in directions:
+                    next_row,next_col=row+dy,col+dx
+                    if valid(next_row,next_col) and (next_row,next_col) not in seen:
+                        seen.add((next_row,next_col))
+                        dfs(next_row,next_col)
+        directions=[(0,1),(1,0),(0,-1),(-1,0)]
+        ans=0
+        seen=set()
+        m=len(grid)
+        n=len(grid[0])
+        for row in range(m):
+            for col in range(n):
+                if grid[row][col]=="1" and (row,col) not in seen:
+                    ans+=1
+                    seen.add((row,col))
+                    dfs(row,col)
+        return ans
+        
