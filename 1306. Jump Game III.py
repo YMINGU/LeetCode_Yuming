2352d8e7a1aa83374dfcdf34a0d52bd3f436c760
Yuming Gu <yug52@pitt.edu>
2024-07-18 12:01:32 -0400

    Update 1306. Jump Game III.py

--- 1306. Jump Game III.py
+++ 1306. Jump Game III.py
@@ -13,4 +13,13 @@ class Solution:
                     q.append(i)
             arr[node]=-arr[node]
         return False
+    -------
+class Solution:
+    def canReach(self, arr: List[int], start: int) -> bool:
+        if 0<=start<len(arr) and arr[start]>=0:
+            if arr[start]==0:
+                return True
+            arr[start]=-arr[start]
+            return self.canReach(arr,start+arr[start]) or self.canReach(arr,start-arr[start])
+        return False
         
