e5ef424e070021c844691f3265e7ff4b509f9e56
Yuming Gu <yug52@pitt.edu>
2024-07-22 12:39:49 -0400

    Create 502. IPO.py

---
+++ 502. IPO.py
@@ -0,0 +1,16 @@
+from heapq import *
+class Solution:
+    def findMaximizedCapital(self, k: int, w: int, profits: List[int], capital: List[int]) -> int:
+        n=len(profits)
+        projects=sorted(zip(capital,profits))
+        heap=[]
+        i=0
+        for _ in range(k):
+            while i<n and projects[i][0]<=w:
+                heappush(heap,-projects[i][1])
+                i+=1
+            if len(heap)==0:
+                return w
+            w-=heappop(heap)
+        return w
+        
