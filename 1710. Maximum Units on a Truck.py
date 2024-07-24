706e1a2ca2ae7967fda332a8756c61f05d02797b
Yuming Gu <yug52@pitt.edu>
2024-07-24 12:56:16 -0400

    Create 1710. Maximum Units on a Truck.py

---
+++ 1710. Maximum Units on a Truck.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maximumUnits(self, boxTypes: List[List[int]], truckSize: int) -> int:
+        boxTypes.sort(key=lambda x:x[1],reverse=True)
+        ans=0
+        for i in boxTypes:
+            if (i[0]<=truckSize):
+                ans+=i[0]*i[1]
+                truckSize-=i[0]
+            else:
+                ans+=truckSize*i[1]
+                break
+        return ans
+        
