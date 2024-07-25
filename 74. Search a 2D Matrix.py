d402e601b4e4c53f0cd1eaf94c20ee9bee17bbb3
Yuming Gu <yug52@pitt.edu>
2024-07-25 13:33:47 -0400

    Create 74. Search a 2D Matrix.py

---
+++ 74. Search a 2D Matrix.py
@@ -0,0 +1,18 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        m,n=len(matrix),len(matrix[0])
+        left=0
+        right=m*n-1
+        while left<=right:
+            mid=(left+right)//2
+            row=mid//n
+            col=mid%n
+            num=matrix[row][col]
+            if num==target:
+                return True
+            elif num<target:
+                left=mid+1
+            else:
+                right=mid-1
+        return False
+        
