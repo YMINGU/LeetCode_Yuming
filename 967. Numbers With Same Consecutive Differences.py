f8654f3b6f2985f58fde33ee8ef6f7fd7ff9a657
Yuming Gu <yug52@pitt.edu>
2024-07-30 14:23:41 -0400

    Update 967. Numbers With Same Consecutive Differences.py

--- 967. Numbers With Same Consecutive Differences.py
+++ 967. Numbers With Same Consecutive Differences.py
@@ -17,3 +17,38 @@ class Solution:
         for h in range(1,10):
             backtrack([str(h)],h)
         return ans
+        -----
+class Solution:
+    def numsSameConsecDiff(self, n: int, k: int) -> List[int]:
+        if n==1:
+            return [i for i in range(10)]
+        ans=[]
+        def dfs(n,num):
+            if n==0:
+                return ans.append(num)
+            digit=num%10
+            next_digits=set([digit+k,digit-k])
+            for next_digit in next_digits:
+                if 0<=next_digit<10:
+                    new_num=num*10+next_digit
+                    dfs(n-1,new_num)
+        for num in range(1,10):
+            dfs(n-1,num)
+        return list(ans)
+        ----
+class Solution:
+    def numsSameConsecDiff(self, n: int, k: int) -> List[int]:
+        if n==1:
+            return [i for i in range(10)]
+        queue=[digit for digit in range(1,10)]
+        for level in range(n-1):
+            next_queue=[]
+            for num in queue:
+                digit=num%10
+                next_digits=set([digit+k,digit-k])
+                for next_digit in next_digits:
+                    if 0<=next_digit<10:
+                        new_num=num*10+next_digit
+                        next_queue.append(new_num)
+            queue=next_queue
+        return queue
