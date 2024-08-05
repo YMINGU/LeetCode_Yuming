714442c6a253439c4b9f7661fa796550506651db
Yuming Gu <yug52@pitt.edu>
2024-08-04 20:46:54 -0400

    Create 62. Unique Paths.py

---
+++ 62. Unique Paths.py
@@ -0,0 +1,28 @@
+from functools import cache
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        @cache
+        def dp(row,col):
+            if row+col==0:
+                return 1
+            ans=0
+            if row>0:
+                ans+=dp(row-1,col)
+            if col>0:
+                ans+=dp(row,col-1)
+            return ans
+        return dp(m-1,n-1)
+  -----
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp=[[0]*(n+1) for _ in range(m+1)]
+        dp[0][0]=1
+        for row in range(m):
+            for col in range(n):
+                if row>0:
+                    dp[row][col]+=dp[row-1][col]
+                if col>0:
+                    dp[row][col]+=dp[row][col-1]
+        return dp[m-1][n-1]
+        
+        
