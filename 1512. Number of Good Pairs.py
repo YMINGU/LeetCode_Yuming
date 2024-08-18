d1920b2094e8148a693db56a12983fdf81eff95e
Yuming Gu <yug52@pitt.edu>
2024-08-18 16:35:02 +0800

    Create 1512. Number of Good Pairs.py

---
+++ 1512. Number of Good Pairs.py
@@ -0,0 +1,10 @@
+from collections import defaultdict
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        counts=defaultdict(int)
+        ans=0
+        for num in nums:
+            ans+=counts[num]
+            counts[num]+=1
+        return ans
+        
