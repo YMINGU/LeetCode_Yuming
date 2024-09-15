121827a22c72f162e33e3b5e486cbce90398ec1d
Yuming Gu <yug52@pitt.edu>
2024-09-14 22:53:19 -0400

    Create 2305. Fair Distribution of Cookies.py

---
+++ 2305. Fair Distribution of Cookies.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def distributeCookies(self, cookies, k):
+        """
+        :type cookies: List[int]
+        :type k: int
+        :rtype: int
+        """
+        cur=[0]*k
+        n=len(cookies)
+        def dfs(i,zero_count):
+            if n-i<zero_count:
+                return float('inf')
+            if i==n:
+                return max(cur)
+            answer=float("inf")
+            for j in range(k):
+                zero_count-=int(cur[j]==0)
+                cur[j]+=cookies[i]
+                answer=min(answer,dfs(i+1,zero_count))
+                cur[j]-=cookies[i]
+                zero_count+=int(cur[j]==0)
+            return answer
+        return dfs(0,k)
+        
