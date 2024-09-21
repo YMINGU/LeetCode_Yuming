a31cee5c1bf3512ab65cfec75ce8bd108b8b5785
Yuming Gu <yug52@pitt.edu>
2024-09-21 15:58:28 -0400

    Update 283. Move Zeroes.py

--- 283. Move Zeroes.py
+++ 283. Move Zeroes.py
@@ -7,5 +7,17 @@ class Solution:
                     nums[j]=nums[i]
                     nums[i]=0
                 j+=1
-            
+-----
+        
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        left=0
+        for right in range(len(nums)):
+            if nums[right]!=0:
+                nums[left],nums[right]=nums[right],nums[left]
+                left+=1
+        return nums
         
