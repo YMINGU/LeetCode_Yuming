69ffb77d1cbaf207daa7fb6029b97761eac271ef
Yuming Gu <yug52@pitt.edu>
2024-10-20 21:33:34 -0400

    Update 222. Count Complete Tree Nodes.py

--- 222. Count Complete Tree Nodes.py
+++ 222. Count Complete Tree Nodes.py
@@ -11,4 +11,16 @@ class Solution:
                 return 0
             return 1+dfs(node.left)+dfs(node.right)
         return dfs(root)
+-------
+
+        
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def countNodes(self, root: Optional[TreeNode]) -> int:
+        return 1+self.countNodes(root.left)+self.countNodes(root.right) if root else 0
         
