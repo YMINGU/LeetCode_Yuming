36811c069dd970dab2a7ad4967b14e887c887588
Yuming Gu <yug52@pitt.edu>
2024-11-21 21:57:01 -0500

    Create 1565. Unique Orders and Customers Per Month.sql

---
+++ 1565. Unique Orders and Customers Per Month.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+SELECT SUBSTRING(order_date,1,7) AS month,COUNT(order_id) AS order_count,COUNT(DISTINCT customer_id) AS customer_count FROM Orders WHERE invoice>20 GROUP BY month
