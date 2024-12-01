6ee1382f04b44ac42097af533ae258d3ba4a9e3e
Yuming Gu <yug52@pitt.edu>
2024-12-01 18:51:34 -0500

    Create 2329. Product Sales Analysis V.sql

---
+++ 2329. Product Sales Analysis V.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+SELECT s.user_id,SUM(s.quantity*p.price) AS spending FROM Sales s JOIN Product p ON s.product_id=p.product_id GROUP BY user_id ORDER BY 2 DESC,1 ASC
