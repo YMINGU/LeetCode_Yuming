cc643efae489726881bbff0a92eadb35c4ab294d
Yuming Gu <yug52@pitt.edu>
2024-12-04 21:05:39 -0500

    Create 1677. Product's Worth Over Invoices.sql

---
+++ 1677. Product's Worth Over Invoices.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+SELECT p.name,IFNULL(SUM(rest),0) AS rest,IFNULL(SUM(paid),0) AS paid,IFNULL(SUM(canceled),0) AS canceled,IFNULL(SUM(refunded),0) AS refunded FROM Product p LEFT JOIN Invoice i ON p.product_id=i.product_id GROUP BY p.name ORDER BY 1 
