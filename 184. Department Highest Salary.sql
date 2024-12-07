10c82cff94ad597e7997562492397c3c4236256f
Yuming Gu <yug52@pitt.edu>
2024-12-06 20:48:39 -0500

    Create 184. Department Highest Salary.sql

---
+++ 184. Department Highest Salary.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+SELECT d.name AS Department,e.name AS Employee,Salary FROM  Employee e JOIN Department d ON e.departmentId=d.id WHERE (e.DepartmentId,Salary) IN (SELECT DepartmentId,MAX(Salary) FROM Employee GROUP BY DepartmentId)
