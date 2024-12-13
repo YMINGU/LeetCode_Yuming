b62a11eb0f2742feca0a93330a449c5a782cf694
Yuming Gu <yug52@pitt.edu>
2024-12-12 22:44:58 -0500

    Create 578. Get Highest Answer Rate Question.sql

---
+++ 578. Get Highest Answer Rate Question.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+SELECT question_id AS survey_log FROM surveylog GROUP BY question_id ORDER BY SUM(CASE WHEN action='answer' THEN 1 ELSE 0 END)/SUM(CASE WHEN action='show' THEN 1 ELSE 0 END) DESC,question_id ASC LIMIT 1
