e4ceddee8deeb483c9b55cd58212d1baf02fa55b
Yuming Gu <yug52@pitt.edu>
2024-12-17 22:50:12 -0500

    Create 1126. Active Businesses.sql

---
+++ 1126. Active Businesses.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+SELECT business_id FROM (SELECT business_id,event_type,occurrences,AVG(occurrences) OVER (PARTITION BY event_type) AS avg FROM Events) t0 WHERE occurrences>avg GROUP BY business_id HAVING COUNT(*)>1
