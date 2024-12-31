66a87fff8ecde016443ae2dfe8740c72f3fddbd5
Yuming Gu <yug52@pitt.edu>
2024-12-30 20:18:28 -0500

    Create 1613. Find the Missing IDs.sql

---
+++ 1613. Find the Missing IDs.sql
@@ -0,0 +1,3 @@
+# Write your MySQL query statement below
+WITH RECURSIVE seq(n) AS (SELECT 1 UNION ALL SELECT n+1 FROM seq WHERE n+1<=(SELECT MAX(Customer_id) FROM Customers))
+SELECT n AS ids FROM seq WHERE n NOT IN (SELECT DISTINCT customer_id FROM Customers)
